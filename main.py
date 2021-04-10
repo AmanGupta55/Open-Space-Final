f3ce7c2bfa121611ffd7d1b5989d3c08b5de99b6
AmanGupta55 <amangupta.seattle@gmail.com>
2021-04-10 14:11:08 -0700

    adding first code

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request, render_template, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def calendar():
+    return render_template("json.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/data')
+def return_data():
+    start_date = request.args.get('start', '')
+    end_date = request.args.get('end', '')
+
+    with open("events.json", "r") as input_data:
+        return input_data.read()
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
